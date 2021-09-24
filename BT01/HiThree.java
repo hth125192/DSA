a26140e805e7e32038febdfda2aab9ce086f2c92
Nguyễn Văn Hưng <20021370@vnu.edu.vn>
2021-09-24 15:14:01 +0700

    DSA

---
+++ BT01/HiThree.java
@@ -0,0 +1,5 @@
+public class HiThree {
+    public static void main(String []args) {
+        System.out.println("Hi " + args[2] + ", " + args[1] + ", and " + args[0] + ".");
+    }
+}
---
+++ BT01/TenHelloWorlds.java
@@ -0,0 +1,14 @@
+public class TenHelloWorlds {
+    public static void main(String []args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+}
