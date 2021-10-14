799351a917c42060a699a542259351a8ad81ceee
Nguyễn Văn Hưng <20021370@vnu.edu.vn>
2021-10-14 23:42:24 +0700

    DSA

---
+++ BT05/BalancedBrackets/BalancedBrackets.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class BalancedBrackets {
+    public static String isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') stack.push(s.charAt(i));
+            else {
+                if (stack.isEmpty()) return "NO";
+                char bracket = stack.pop();
+                if ((s.charAt(i) == ')' && bracket != '(') || (s.charAt(i) == ']' && bracket != '[') || (s.charAt(i) == '}' && bracket != '{')) return "NO";
+            }
+        }
+        if (stack.isEmpty()) return "YES";
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBalanced("{[(())]()(){[{}]}[]{()}}()"));
+    }
+
+}
